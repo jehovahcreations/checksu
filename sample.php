6e6ff303c190c083ec860d12d695f4fc55ded521
serafad08@gmail.com <serafad08@gmail.com>
2022-01-02 04:15:20 +0530

    dodklkggll

--- sample.php
+++ sample.php
@@ -6,28 +6,28 @@ require_once("./PaytmChecksum.php");
 $paytmParams = array();
 
 /* add parameters in Array */
-$paytmParams["MID"] = "XVNIki66916401398815";
+$paytmParams["MID"] = "NBXLHA89674602386110";
 $paytmParams["ORDERID"] = "5555";
 
 /**
 * Generate checksum by parameters we have
 * Find your Merchant Key in your Paytm Dashboard at https://dashboard.paytm.com/next/apikeys 
 */
-$paytmChecksum = PaytmChecksum::generateSignature($paytmParams, 'n1CIsHE5hcmzNhMW');
-$verifySignature = PaytmChecksum::verifySignature($paytmParams, 'n1CIsHE5hcmzNhMW', $paytmChecksum);
+$paytmChecksum = PaytmChecksum::generateSignature($paytmParams, '@9c3YZKKTvIbWbAa');
+$verifySignature = PaytmChecksum::verifySignature($paytmParams, '@9c3YZKKTvIbWbAa', $paytmChecksum);
 //echo sprintf("generateSignature Returns: %s\n", $paytmChecksum);
 //echo sprintf("verifySignature Returns: %b\n\n", $verifySignature);
 
 
 /* initialize JSON String */  
-$body = "{\"mid\":\"XVNIki66916401398815\",\"orderId\":\"5555\"}";
+$body = "{\"mid\":\"NBXLHA89674602386110\",\"orderId\":\"5555\"}";
 
 /**
 * Generate checksum by parameters we have in body
 * Find your Merchant Key in your Paytm Dashboard at https://dashboard.paytm.com/next/apikeys 
 */
-$paytmChecksum = PaytmChecksum::generateSignature($body, 'n1CIsHE5hcmzNhMW');
-$verifySignature = PaytmChecksum::verifySignature($body, 'n1CIsHE5hcmzNhMW', $paytmChecksum);
+$paytmChecksum = PaytmChecksum::generateSignature($body, '@9c3YZKKTvIbWbAa');
+$verifySignature = PaytmChecksum::verifySignature($body, '@9c3YZKKTvIbWbAa', $paytmChecksum);
 echo sprintf($paytmChecksum);
 //echo sprintf("verifySignature Returns: %b\n\n", $verifySignature);
 //$myArr = array("checksum"=>$paytmChecksum, "verified"=>$verifySignature);
