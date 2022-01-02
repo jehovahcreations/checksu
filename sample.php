3a71a97855e596e2da01c681799921f0dc21c5d0
serafad08@gmail.com <serafad08@gmail.com>
2022-01-02 21:56:21 +0530

    dsskl

--- sample.php
+++ sample.php
@@ -13,21 +13,24 @@ $paytmParams["ORDERID"] = $_GET['order'];
  * Generate checksum by parameters we have
  * Find your Merchant Key in your Paytm Dashboard at https://dashboard.paytm.com/next/apikeys 
  */
-$paytmChecksum = PaytmChecksum::generateSignature($paytmParams, $_GET['key']);
-$verifySignature = PaytmChecksum::verifySignature($paytmParams, $_GET['key'], $paytmChecksum);
-echo sprintf($paytmChecksum);
+// $paytmChecksum = PaytmChecksum::generateSignature($paytmParams, $_GET['key']);
+// $verifySignature = PaytmChecksum::verifySignature($paytmParams, $_GET['key'], $paytmChecksum);
+
+// echo json_encode({})
 // echo sprintf("generateSignature Returns: %s\n", $paytmChecksum);
 // echo sprintf("verifySignature Returns: %b\n\n", $verifySignature);
 
-// $json = array("mid"=> $_GET['mid'],"orderId"=> $_GET['order']);
-// /* initialize JSON String */
-// $body = json_encode($json);
+$json = array("mid"=> $_GET['mid'],"orderId"=> $_GET['order']);
+/* initialize JSON String */
+$body = json_encode($json);
 
-// /**
-//  * Generate checksum by parameters we have in body
-//  * Find your Merchant Key in your Paytm Dashboard at https://dashboard.paytm.com/next/apikeys 
-//  */
-// $paytmChecksum = PaytmChecksum::generateSignature($body, $_GET['key']);
-// $verifySignature = PaytmChecksum::verifySignature($body, $_GET['key'], $paytmChecksum);
+/**
+ * Generate checksum by parameters we have in body
+ * Find your Merchant Key in your Paytm Dashboard at https://dashboard.paytm.com/next/apikeys 
+ */
+$paytmChecksum = PaytmChecksum::generateSignature($body, $_GET['key']);
+$verifySignature = PaytmChecksum::verifySignature($body, $_GET['key'], $paytmChecksum);
+$jaas = array("CHECKSUMHASH"=>$paytmChecksum);
+echo json_encode($jaas);
 // echo sprintf("generateSignature Returns: %s\n", $paytmChecksum);
 // echo sprintf("verifySignature Returns: %b\n\n", $verifySignature);
