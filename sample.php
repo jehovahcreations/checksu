b02f6d5fa1a0cc33301555a6c8fadf5653752f6f
serafad08@gmail.com <serafad08@gmail.com>
2022-01-02 21:45:43 +0530

    dssk

--- sample.php
+++ sample.php
@@ -13,20 +13,21 @@ $paytmParams["ORDERID"] = $_GET['order'];
  * Generate checksum by parameters we have
  * Find your Merchant Key in your Paytm Dashboard at https://dashboard.paytm.com/next/apikeys 
  */
-// $paytmChecksum = PaytmChecksum::generateSignature($paytmParams, 'YOUR_MERCHANT_KEY');
-// $verifySignature = PaytmChecksum::verifySignature($paytmParams, 'YOUR_MERCHANT_KEY', $paytmChecksum);
+$paytmChecksum = PaytmChecksum::generateSignature($paytmParams, $_GET['key']);
+$verifySignature = PaytmChecksum::verifySignature($paytmParams, $_GET['key'], $paytmChecksum);
+return json_encode($paytmChecksum,JSON_UNESCAPED_UNICODE);
 // echo sprintf("generateSignature Returns: %s\n", $paytmChecksum);
 // echo sprintf("verifySignature Returns: %b\n\n", $verifySignature);
 
-$json = array("mid"=> $_GET['mid'],"orderId"=> $_GET['order']);
-/* initialize JSON String */
-$body = json_encode($json);
+// $json = array("mid"=> $_GET['mid'],"orderId"=> $_GET['order']);
+// /* initialize JSON String */
+// $body = json_encode($json);
 
-/**
- * Generate checksum by parameters we have in body
- * Find your Merchant Key in your Paytm Dashboard at https://dashboard.paytm.com/next/apikeys 
- */
-$paytmChecksum = PaytmChecksum::generateSignature($body, $_GET['key']);
-$verifySignature = PaytmChecksum::verifySignature($body, $_GET['key'], $paytmChecksum);
-echo sprintf($paytmChecksum);
-//echo sprintf("verifySignature Returns: %b\n\n", $verifySignature);
+// /**
+//  * Generate checksum by parameters we have in body
+//  * Find your Merchant Key in your Paytm Dashboard at https://dashboard.paytm.com/next/apikeys 
+//  */
+// $paytmChecksum = PaytmChecksum::generateSignature($body, $_GET['key']);
+// $verifySignature = PaytmChecksum::verifySignature($body, $_GET['key'], $paytmChecksum);
+// echo sprintf("generateSignature Returns: %s\n", $paytmChecksum);
+// echo sprintf("verifySignature Returns: %b\n\n", $verifySignature);
